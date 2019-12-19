e706cfdff5a1407c637fcf6c37cfed15f186b250
TLanghorne <tresvt@gmail.com>
2019-12-19 16:11:05 -0500

    change to functional component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,18 +2,17 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import PropTypes from "prop-types"; //Specifically this line
 
-class Todos extends React.Component {
-  render() {
-    return this.props.todos.map(todo => (
-      <TodoItem
-        key={todo.id}
-        todo={todo}
-        markComplete={this.props.markComplete}
-        delTodo={this.props.delTodo}
-      />
-    ));
-  }
-}
+const Todos = () => {
+  return this.props.todos.map(todo => (
+    <TodoItem
+      key={todo.id}
+      todo={todo}
+      markComplete={this.props.markComplete}
+      delTodo={this.props.delTodo}
+    />
+  ));
+};
+
 Todos.propTypes = {
   // and also this section
   todos: PropTypes.array.isRequired
